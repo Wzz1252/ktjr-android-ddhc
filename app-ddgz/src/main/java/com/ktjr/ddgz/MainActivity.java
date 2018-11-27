5a4747f309a46100f0a25aaa18e91fd7801591f5
Torment <13120201339@qq.com>
2018-11-27 23:35:39 +0800

    初始化项目

---
+++ app-ddgz/src/main/java/com/ktjr/ddgz/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ktjr.ddgz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ktjr.ddgz.ktjr_android_ddhc.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
